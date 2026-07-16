73f399a228bbfacd8f7de38a6315f290f8697871
Gillian Tunney <gbtunney@mac.com>
2026-07-16 00:17:00 -0400

    feat(template-example-react): adding react repo

---
+++ packages/example-react/src/index.ts
@@ -0,0 +1,3 @@
+export { Button } from './components/Button.tsx'
+export { Header } from './components/Header.tsx'
+export { Page } from './components/Page.tsx'
---
+++ packages/example-react/src/stories/Button.stories.ts
@@ -0,0 +1,54 @@
+import type { Meta, StoryObj } from '@storybook/react-vite'
+import { fn } from 'storybook/test'
+
+import { Button } from './../components/Button.tsx'
+
+// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
+const meta = {
+    // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#story-args
+    args: { onClick: fn() },
+    // More on argTypes: https://storybook.js.org/docs/api/arg-types
+    argTypes: {
+        backgroundColor: { control: 'color' },
+    },
+    component: Button,
+    parameters: {
+        // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
+        layout: 'centered',
+    },
+    // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
+    tags: ['autodocs'],
+    title: 'Example/Button',
+} satisfies Meta<typeof Button>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
+export const primary: Story = {
+    args: {
+        backgroundColor: 'blue',
+        label: 'Button',
+        primary: true,
+    },
+}
+
+export const secondary: Story = {
+    args: {
+        label: 'Button',
+    },
+}
+
+export const large: Story = {
+    args: {
+        label: 'Button',
+        size: 'large',
+    },
+}
+
+export const small: Story = {
+    args: {
+        label: 'Button',
+        size: 'small',
+    },
+}
